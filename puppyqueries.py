dbbbd1df870b4846c99691fe38b2c42b37980d6e
Ryan Connor <ryanwc13@gmail.com>
2016-03-20 19:13:53 +0700

    fixed bug in getYoungestPuppies

--- puppyqueries.py
+++ puppyqueries.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 from puppy_db_setup import Base, Shelter, Puppy
 
+import datetime
+
 
 def getPuppyDBSession():
     """Return an interactive session with the puppy adoption database
@@ -17,7 +19,7 @@ def getPuppiesByName():
     """Return all puppies in ascending alphabetical order
     """
     session = getPuppyDBSession()
-    puppiesByName = session.query(Puppy).order_by(name)
+    puppiesByName = session.query(Puppy).order_by(name).all()
     session.close()
     return puppiesByName
 
@@ -26,7 +28,8 @@ def getYoungestPuppies():
     """
     session = getPuppyDBSession()
     today = datetime.date.today()
-    youngestPuppies = session.query(Puppy).filter(Puppy.dateOfBirth-today<=182).order_by(dateOfBirth)
+    sixMonthsAgo = today - datetime.timedelta(days = 182)
+    youngestPuppies = session.query(Puppy).filter(Puppy.dateOfBirth<=sixMonthsAgo).order_by(dateOfBirth).()
     session.close()
     return youngestPuppies
 
@@ -34,7 +37,7 @@ def getPuppiesByWeight():
     """Retrun all puppies in ascending order by weight
     """
     session = getPuppyDBSession()
-    puppiesByWeight = session.query(Puppy).order_by(weight)
+    puppiesByWeight = session.query(Puppy).order_by(weight).all()
     session.close()
     return puppiesByWeight
 
@@ -42,6 +45,6 @@ def getPuppiesByShelter():
     """Return all puppies grouped by shelter
     """
     session = getPuppyDBSession()
-    
+    puppiesBySheler = session.query(Puppy).group_by(shelter_id)
     session.close()
     return
