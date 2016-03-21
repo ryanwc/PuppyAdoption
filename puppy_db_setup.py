bbe9954251c0c471a6186712f7243dbe775204e8
Ryan Connor <ryanwc13@gmail.com>
2016-03-21 12:18:03 +0700

    expand schema to manage adopters

--- puppy_db_setup.py
+++ puppy_db_setup.py
@@ -2,6 +2,7 @@ import sys
 
 # import functionality from sqlalchemy
 from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
+from sqlalchemy.schema import PrimaryKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -13,27 +14,67 @@ Base = declarative_base()
 
 # define tables for the database in Python classes
 class Shelter(Base):
-	__tablename__ = 'shelter'
+        __tablename__ = 'shelter'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	address = Column(String(250), nullable=False)
-	city = Column(String(80), nullable=False)
-	state = Column(String(30), nullable=False)
-	zipCode = Column(String(10), nullable=False)
-	website = Column(String)
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250), nullable=False)
+        address = Column(String(250), nullable=False)
+        city = Column(String(80), nullable=False)
+        state = Column(String(30), nullable=False)
+        zipCode = Column(String(10), nullable=False)
+        website = Column(String)
+
+        puppies = relationship("Puppy")
+
 
 class Puppy(Base):
-	__tablename__ = 'puppy'
+        __tablename__ = 'puppy'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
-	dateOfBirth = Column(Date)
-	gender = Column(String(6), nullable = False)
-	weight = Column(Numeric(10))
-	picture = Column(String)
-	shelter_id = Column(Integer, ForeignKey('shelter.id'))
-	shelter = relationship(Shelter)
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250), nullable=False)
+        dateOfBirth = Column(Date)
+        gender = Column(String(6), nullable = False)
+        weight = Column(Numeric(10))
+        picture = Column(String)
+        shelter_id = Column(Integer, ForeignKey('shelter.id'))
+
+        adopter = relationship("Adopter", uselist=False, back_populates="puppy")
+
+
+class Adopter(Base):
+        __tablename__ = 'adopter'
+        __table_args__ = (
+                PrimaryKeyConstraint('person_id', 'puppy_id'),
+        )
+
+        person_id = Column(Integer, ForeignKey('person.id'))
+        puppy_id = Column(Integer, ForeignKey('puppy.id'))
+
+        puppy = relationship("Puppy", back_populates="Puppy")
+
+
+class Person(Base):
+        __tablename__ = 'person'
+
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250), nullable=False)
+        household_id = Column(Integer, ForeignKey('household.id'))
+        #headOfHousehold = Column(Boolean, nullable=False)
+
+        adoptions = relationship("Adopter")
+
+
+class Household(Base):
+        __tablename__ = 'household'
+
+        id = Column(Integer, primary_key=True)
+        #headOfHousehould = Column(Integer, ForeignKey('person.id'))
+        address = Column(String(250), nullable=False)
+        city = Column(String(80), nullable=False)
+        state = Column(String(30), nullable=False)
+        zipCode = Column(String(10), nullable=False)
+
+        members = relationship("Person")
 
 
 # connect to database engine
