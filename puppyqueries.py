be39006edabe0f9a4a0b5f8a7b24bddf68e52fb1
Ryan Connor <ryanwc13@gmail.com>
2016-03-22 23:27:40 +0700

    get number of puppies by shelter; clean up spacing

--- puppyqueries.py
+++ puppyqueries.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, func, distinct
 from sqlalchemy.orm import sessionmaker
 
 from puppy_db_setup import Base, Shelter, Puppy
@@ -16,35 +16,58 @@ def getPuppyDBSession():
     return session
 
 def getPuppiesByName():
-    """Return all puppies in ascending alphabetical order
+    """Return a list all puppies in ascending alphabetical order
     """
     session = getPuppyDBSession()
+    
     puppiesByName = session.query(Puppy).order_by(Puppy.name)
+    
     session.close()
     return puppiesByName
 
 def getYoungestPuppies():
-    """Return all puppies less than 6 months old in ascending order by age
+    """Return a list of all puppies less than 6 months old in ascending
+        order by age
     """
     session = getPuppyDBSession()
+    
     today = datetime.date.today()
     sixMonthsAgo = today - datetime.timedelta(days = 182)
-    youngestPuppies = session.query(Puppy).filter(Puppy.dateOfBirth>=sixMonthsAgo).order_by(Puppy.dateOfBirth)
+    youngestPuppies = session.query(Puppy).\
+                      filter(Puppy.dateOfBirth>=sixMonthsAgo).\
+                      order_by(Puppy.dateOfBirth)
+
     session.close()
     return youngestPuppies
 
 def getPuppiesByWeight():
-    """Retrun all puppies in ascending order by weight
+    """Retrun a list of all puppies in ascending order by weight
     """
     session = getPuppyDBSession()
+
     puppiesByWeight = session.query(Puppy).order_by(Puppy.weight)
+
     session.close()
     return puppiesByWeight
 
 def getPuppiesByShelter():
-    """Return all puppies ordered by shelter
+    """Return a list of all puppies ordered by shelter
     """
     session = getPuppyDBSession()
+
     puppiesByShelter = session.query(Puppy).order_by(Puppy.shelter_id)
+
     session.close()
     return puppiesByShelter
+
+def getNumPuppiesByShelter():
+    """Return a list with number of puppies in each shelter
+    """
+    session getPuppyDBSession()
+
+    numPuppiesByShelter = session.query(func.count(Puppy.name)).\
+                          group_by(Puppy.shelter_id)
+
+    session.close()
+    return numPuppiesByShelter
+
