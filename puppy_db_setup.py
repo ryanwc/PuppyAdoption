31ed81d5eede04bf265615c011065f06d150c945
Ryan Connor <ryanwc13@gmail.com>
2016-03-23 13:23:00 +0700

    change a data type

--- puppy_db_setup.py
+++ puppy_db_setup.py
@@ -22,7 +22,7 @@ class Shelter(Base):
         city = Column(String(80), nullable=False)
         state = Column(String(30), nullable=False)
         zipCode = Column(String(10), nullable=False)
-        capacity = Column(Integer, nullable=False)
+        capacity = Column(Numeric(10), nullable=False)
         website = Column(String)
 
         puppies = relationship("Puppy")
