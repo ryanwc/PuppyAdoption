ceaaadab67c25c62331451f135e81e025f96672b
Ryan Connor <ryanwc13@gmail.com>
2016-03-23 16:39:48 +0700

    load balancing add puppy method passes test for one additional puppy

--- puppymanager.py
+++ puppymanager.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 
 from puppy_db_setup import Base, Shelter, Puppy
-import puppyqueries
+from puppyqueries import getPupDBSession, getSheltFullness, getNumPupsByShelt
 
 import time, datetime
 
@@ -17,21 +17,20 @@ def addPuppyToShelter(PuppyInfo):
       True if puppy was successfully added to the database
       False if puppy was not added to the database
     """
-    session = getPuppyDBSession()
+    session = getPupDBSession()
 
     # prep steps; find best shelter based on fullness
-    numPuppiesByShelter = puppyqueries.getNumPuppiesByShelter()
-    shelterIDs = [shelter.shelter_id for shelter in numPuppiesByShelter]
-    shelterFullness = puppyqueries.getShelterFullness()
+    numPuppiesByShelter = getNumPupsByShelt()
+    shelterIDs = [shelter['shelter_id'] for shelter in numPuppiesByShelter]
+    shelterFullness = getSheltFullness()
 
     leastFilledShelter = []
 
     for shelter in shelterFullness:
-        if shelter[1] < 1:
+        if shelter['percentFull'] < 1:
             if (len(leastFilledShelter) == 0 or
-                shelter[1] < leastFilledShelter[1]):
-                leastFilledShelter = [{"shelter_id": shelter[0], \
-                                       "percentFull": shelter[1]}]
+                shelter['percentFull'] < leastFilledShelter['percentFull']):
+                leastFilledShelter = shelter
 
     # fail if all shelters are at capacity
     if len(leastFilledShelter) < 1:
@@ -39,67 +38,68 @@ def addPuppyToShelter(PuppyInfo):
         return False
 
     # test/format name
-    if PuppyInfo.name is None:
+    if 'name' not in PuppyInfo:
         while True:
             print "Puppy name not provided"
             name = raw_input("What is the puppy's name? ")
             if (len(name)>0):
                 break
-        PuppyInfo.append(name)
+        PuppyInfo['name'] = name
 
     while True:
-        if (not isinstance(PuppyInfo.name,str) or
-            len(PuppyInfo.name) > 250 or
-            len(PuppyInfo.name) < 1):
+        if (not isinstance(PuppyInfo['name'],str) or
+            len(PuppyInfo['name']) > 250 or
+            len(PuppyInfo['name']) < 1):
             print "Puppy name not in right format"
-            PuppyInfo.name = raw_input("What is the puppy's name? ")
+            PuppyInfo['name'] = raw_input("What is the puppy's name? ")
         else:
             break
 
     # test/format gender
-    if PuppyInfo.gender is None:
+    if 'gender' not in PuppyInfo:
         while True:
             print "Puppy gender not provided"
             gender = raw_input("What is the puppy's gender? ")
             if (len(gender)>0):
                 break
-        PuppyInfo.append(gender)
+        PuppyInfo['gender'] = gender
         
     while True:
-        if (not isinstance(PuppyInfo.gender,str) or
-            len(PuppyInfo.gender) > 6 or
-            len(PuppyInfo.gender) < 1):
+        if (not isinstance(PuppyInfo['gender'],str) or
+            len(PuppyInfo['gender']) > 6 or
+            len(PuppyInfo['gender']) < 1):
             print "Puppy gender not provided or not in right format"
-            PuppyInfo.gender = raw_input("What is the puppy's gender? ")
+            PuppyInfo['gender'] = raw_input("What is the puppy's gender? ")
         else:
             break
 
     # test/format date of birth
     while True:
-        if PuppyInfo.dateOfBirth is None:
+        if 'dateOfBirth' not in PuppyInfo:
+            PuppyInfo['dateOfBirth'] = None
             break
-        else if not isinstance(PuppyInfo.dateOfBirth,datetime):
+        elif not isinstance(PuppyInfo['dateOfBirth'],datetime):
             print "Puppy date of birth not in right format"
             dateOfBirthStr = raw_input("What is the puppy's DOB (DD MM YY)? ")
             dateOfBirthStruct = time.strptime(dateOfBirthStr, "%d %m %y")
-            PuppyInfo.dateOfBirth = datetime.\
-                                    fromtimestamp(mktime(dateofBirthStruct))
+            PuppyInfo['dateOfBirth'] = datetime.\
+                                       fromtimestamp(mktime(dateofBirthStruct))
         else:
             break
 
     # test/format picture
     while True:
-        if PuppyInfo.picture is None:
-
+        if 'picture' not in PuppyInfo:
+            PuppyInfo['picture'] = None
             break
-        else if not isinstance(picture, str):
+        elif not isinstance(PuppyInfo['picture'], str):
             print "Picture is not in the right format"
-            PuppyInfo.picture = raw_input("Provide URL to puppy's picture: ")
+            PuppyInfo['picture'] = raw_input("Provide URL to puppy's picture: ")
         else:
             break
 
     # test/format shelter id (including balancing)
-    if PuppyInfo.shelter_id is None:
+    if 'shelter_id' not in PuppyInfo:
         while True:
             print "Shelter ID not provided"
             shelter_str = raw_input("What is the ID of the shelter should the "\
@@ -107,42 +107,51 @@ def addPuppyToShelter(PuppyInfo):
             if (len(shelter_str)>0):
                 break
         shelter_id = int(shelter_str)
-        PuppyInfo.append(shelter_id)
+        PuppyInfo['shelter_id'] = shelter_id
 
     while True:
-        if not isinstance(PuppyInfo.shelter_id,int):
-            print "Shelter ID is invalid"
-        else if not PuppyInfo.shelter_id in shelterIDs:
-            PuppyInfo.shelter_id = int(raw_input("Given shelter ID does not "\
-                                                 "exist, please re-enter: "))
-        else if PuppyInfo.shelter_id != leastFilledShelter[0]:
-            print "Using shelter " + leastFilledShelter[0] + " instead of " \
-                  "shelter " + PuppyInfo.shelter_id + " because shelter "\
-                  + leastFilledShelter[0] + " has the most capacity"
-            PuppyInfo.shelter_id = leastFilledShelter[0]
+        if ( (not isinstance(PuppyInfo['shelter_id'],int)) or
+             (len(str(PuppyInfo['shelter_id'])) < 1) ):
+            PuppyInfo['shelter_id'] = int(raw_input("Shelter ID is in "\
+                                                    "invalid format, please "\
+                                                    "re-enter: "))
+        elif not PuppyInfo['shelter_id'] in shelterIDs:
+            PuppyInfo['shelter_id'] = int(raw_input("Given shelter ID does "\
+                                                    "not exist, please "\
+                                                    "re-enter: "))
+        elif PuppyInfo['shelter_id'] != leastFilledShelter['shelter_id']:
+            print "Using shelter ", leastFilledShelter['shelter_id'], \
+                  " instead of shelter ", PuppyInfo['shelter_id'], \
+                  " because shelter ", leastFilledShelter['shelter_id'], \
+                  " has the most remaining capacity"
+            PuppyInfo['shelter_id'] = leastFilledShelter['shelter_id']
             break
         else:
             break
             
     # test/format weight
     while True:
-        if PuppyInfo.weight is None:
+        if 'weight' not in PuppyInfo:
+            PuppyInfo['weight'] = None
             break
-        else if isinstance(PuppyInfo.weight,str):
-            PuppyInfo.weight = int(PuppyInfo.weight)
-        else if (not isinstance(PuppyInfo.weight,int) or
-                 len(str(PuppyInfo.weight)) > 10 or
-                 len(str(PuppyInfo.weight)) < 1):
+        elif isinstance(PuppyInfo['weight'],str):
+            PuppyInfo['weight'] = int(PuppyInfo['weight'])
+        elif (not isinstance(PuppyInfo['weight'],int) or
+                 len(str(PuppyInfo['weight'])) > 10 or
+                 len(str(PuppyInfo['weight'])) < 1):
             print "Weight is not in the right format"
             weightStr = raw_input("What is the puppy's weight? ")
-            PuppyInfo.weight = int(weightStr)
+            PuppyInfo['weight'] = int(weightStr)
         else:
             break
 
     # insert the new puppy
-    new_puppy = Puppy(PuppyInfo.name,PuppyInfo.gender,\
-                      PuppyInfo.dateOfBirth,PuppyInfo.picture,\
-                      leastFilledShelter[0],PuppyInfo.weight)
+    new_puppy = Puppy(name = PuppyInfo['name'],\
+                      gender = PuppyInfo['gender'],\
+                      dateOfBirth = PuppyInfo['dateOfBirth'],\
+                      picture = PuppyInfo['picture'],\
+                      shelter_id = PuppyInfo['shelter_id'],\
+                      weight = PuppyInfo['weight'])
     session.add(new_puppy)
     session.commit()
     
